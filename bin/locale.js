d3293e09675d0cd0bcc0dc72e81a75764495a451
CalvinRodo <calvinv@gmail.com>
2019-10-28 10:18:48 -0400

    Getting the locale cli to work

--- bin/locale.js
+++ bin/locale.js
@@ -5,20 +5,21 @@ const program = new commander.Command()
 program.version('0.0.1')
 
 program.option('add', 'Add a string to the locale files')
-program.option('--key <type>', 'name of key to add')
+program.option('--key <type>', 'name of key to add, periods must be escaped with \\')
 program.parse(process.argv)
 
 if (program.add) { 
   if(!program.key){ 
     console.error('--key is missing')
+    return
   }
 
   
   let en = ejf("./locales/en.json")
   let fr = ejf("./locales/fr.json")
   
-  en.set(program.key.toString(),program.key)
-  fr.set(program.key.toString(),program.key)
+  en.set(program.key.toString(),program.key.toString().replace("\\",""))
+  fr.set(program.key.toString(),program.key.toString().replace("\\",""))
   en.save()
   fr.save()
 }
