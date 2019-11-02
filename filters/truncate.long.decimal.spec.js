bf77f333b6bad597b2f500f4b4e93aa7c61ba838
Calvin Rodo <calvinrodo@Calvins-ESDC-MacBook-Pro.local>
2019-11-02 18:55:53 -0400

    adding tests fixing page

--- filters/truncate.long.decimal.spec.js
+++ filters/truncate.long.decimal.spec.js
@@ -1,12 +1,24 @@
 const {  truncateDecimal } = require('./truncate.long.decimal')
 
-test('returns a string with 2 decimal places', () => {
+function truncate(num) { 
   const env = {}
   env.filters = {}
   env.addFilter = (name, func) => {
     env.filters[name] = func
   }
   truncateDecimal(env)
-  const attrs = env.filters.truncateDecimal(1.1234,2)
-  expect(attrs).toEqual(1.12)
+  return env.filters.truncateDecimal(num,2)
+
+}
+
+test('returns a string with 2 decimal places', () => {
+  expect(truncate(1.1245)).toEqual(1.12)
+})
+
+test('returns value if no decimal places', () => { 
+  expect(truncate(500)).toEqual(500)
+}) 
+
+test('returns a negative value properly', () => { 
+  expect(truncate(-31432.493535)).toEqual(-31432.49)
 })
